1659563d6c29ea324cf7ecffb402c9869ca96176
John Masiello <vn50b16@homeoffice.wal-mart.com>
2021-04-24 19:24:05 -0500

    Primary Author John Masiello

---
+++ src/test/java/com/example/testKafka/EmbeddedKafkaIntegrationTest.java
@@ -0,0 +1,40 @@
+package com.example.testKafka;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+
+import com.example.testKafka.components.KafkaConsumer;
+import com.example.testKafka.components.KafkaProducer;
+import java.util.concurrent.TimeUnit;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.annotation.DirtiesContext;
+
+@SpringBootTest
+@DirtiesContext
+@EmbeddedKafka(partitions = 1,
+		brokerProperties = { "listeners=PLAINTEXT://localhost:9092", "port=9092" })
+class EmbeddedKafkaIntegrationTest {
+		@Autowired
+    private KafkaConsumer consumer;
+
+    @Autowired
+    private KafkaProducer producer;
+
+    @Value("${test.topic}")
+    private String topic;
+
+    @Test
+    public void givenEmbeddedKafkaBroker_whenSendingToSimpleProducer_thenMessageReceived()
+      throws Exception {
+        producer.send(topic, "Sending with own simple KafkaProducer");
+        consumer.getLatch().await(10000, TimeUnit.MILLISECONDS);
+
+        assertThat(consumer.getLatch().getCount(), equalTo(0L));
+        assertThat(consumer.getPayload(), containsString("embedded-test-topic"));
+    }
+}
